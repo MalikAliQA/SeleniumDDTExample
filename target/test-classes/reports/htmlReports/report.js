2b93f6ef775f946121bb8de523094c801cff40d6
Malik <malik_ali1998@hotmail.co.uk>
2021-05-07 15:31:09 +0100

    updating chrome driver test data requires new emails

--- target/test-classes/reports/htmlReports/report.js
+++ target/test-classes/reports/htmlReports/report.js
@@ -81,7 +81,7 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 8437507600,
+  "duration": 4729336600,
   "status": "passed"
 });
 formatter.scenario({
@@ -111,25 +111,25 @@ formatter.match({
   "location": "UserLogin.i_can_access_the_shopping_site()"
 });
 formatter.result({
-  "duration": 2033182800,
+  "duration": 2275423600,
   "status": "passed"
 });
 formatter.match({
   "location": "UserLogin.i_enter_the_correct_info()"
 });
 formatter.result({
-  "duration": 52462990600,
-  "status": "passed"
+  "duration": 18385595400,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"#header \u003e div.nav \u003e div \u003e div \u003e nav \u003e div:nth-child(1) \u003e a \u003e span\"}\n  (Session info: chrome\u003d89.0.4389.114)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027LAPTOP-24TH8E7N\u0027, ip: \u0027192.168.1.148\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002714.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 89.0.4389.114, chrome: {chromedriverVersion: 89.0.4389.23 (61b08ee2c5002..., userDataDir: C:\\Users\\Malik\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:62076}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 57c17e2b5bedf86e52cd88cef7c09eca\n*** Element info: {Using\u003dcss selector, value\u003d#header \u003e div.nav \u003e div \u003e div \u003e nav \u003e div:nth-child(1) \u003e a \u003e span}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByCssSelector(RemoteWebDriver.java:420)\r\n\tat org.openqa.selenium.By$ByCssSelector.findElement(By.java:431)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat stepdef.UserLogin.i_enter_the_correct_info(UserLogin.java:105)\r\n\tat ✽.When I enter the correct info(src/test/resources/cuke/ShoppingSite.feature:23)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "UserLogin.i_sign_in()"
 });
 formatter.result({
-  "duration": 22500,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "duration": 114085400,
+  "duration": 102751000,
   "status": "passed"
 });
 });
